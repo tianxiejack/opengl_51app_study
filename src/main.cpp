c253ffe514d4ad8dafdce763b1630fe73edac49a
alex <alex@qr.com>
2018-09-11 00:54:17 +0800

    分别画点的形式从三角形到圆形

--- src/main.cpp
+++ src/main.cpp
@@ -17,6 +17,36 @@ typedef struct float3{
 
 void Render()
 {
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+
+	glOrtho(0,_width,_height,0,-100,100);
+
+	float cx = 100;
+	float cy = 100;
+	float cz = 0;
+	float r = 80;
+
+	for(int i=0;i<360;++i)
+	{
+		glBegin(GL_TRIANGLES);
+
+		float x = (float)cos((double)i*M_PI/180)*r + cx;
+		float y = (float)sin((double)i*M_PI/180)*r + cy;
+
+		float x1 = (float)cos((double)(i+1)*M_PI/180)*r + cx;
+		float y1 = (float)sin((double)(i+1)*M_PI/180)*r + cy;
+
+		glVertex3f(cx,cy,cz);
+		glVertex3f(x,y,cz);
+		glVertex3f(x1,y1,cz);
+
+		glEnd();
+
+	}
+
+
+	/*
 	float centerx = 100,centery = 100, centerz = 0;
 	float r = 80;
 
@@ -40,6 +70,7 @@ void Render()
 	glEnableClientState(GL_VERTEX_ARRAY);
 	glVertexPointer(3,GL_FLOAT,0,cir);
 	glDrawArrays(GL_TRIANGLE_FAN,0,362);
+	*/
 }
 
 void myDisplay(void)
@@ -47,16 +78,8 @@ void myDisplay(void)
 	glClearColor(1,0,0,1);
 	glClear(GL_COLOR_BUFFER_BIT);
 	glViewport(0,0,_width,_height);
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	glOrtho(0,_width,_height,0,-100,100);
 
-	glBegin(GL_TRIANGLES);
-	glVertex3f(_width*0.5f,0,0);
-	glVertex3f(_width,_height,0);
-	glVertex3f(0,_height,0);
-	glEnd();
-	//Render();
+	Render();
 	glutSwapBuffers();
 }
 

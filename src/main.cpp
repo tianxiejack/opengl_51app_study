d4c3b7ee0602e94157b8fef7c5c125a0019ecd23
alex <alex@qr.com>
2018-09-18 07:02:45 +0800

    4-2 freeimage

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 #include "freetype.hpp"
 #include <math.h>
 #include <glu.h>
+#include "FreeImage.h"
 
 FreeTypeFont*	_font;
 FreeTypeFont*	_font43;
@@ -29,21 +30,57 @@ void SamplerDrawPixel()
 	}
 }
 
-void    onInitGL()
- {
-     glEnable(GL_TEXTURE_2D);
-     glGenTextures(1,&_texture);
-     glBindTexture(GL_TEXTURE_2D,_texture);
-     char*   data    =   new char[128 * 128 * 4];
-     for (int i = 0 ;i < 128 * 128 * 4 ; ++ i)
-     {
-         data[i] =   rand()%255;
-     }
-     glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_LINEAR);
-     glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR);
-     glTexImage2D(GL_TEXTURE_2D,0,GL_RGBA,128,128,0,GL_RGBA,GL_UNSIGNED_BYTE,data);
+unsigned    createTexture(int w,int h,const void* data)
+{
+    unsigned    texId;
+    glGenTextures(1,&texId);
+    glBindTexture(GL_TEXTURE_2D,texId);
+    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR);
+    glTexImage2D(GL_TEXTURE_2D,0,GL_RGBA,w,h,0,GL_RGBA,GL_UNSIGNED_BYTE,data);
 
- }
+    return  texId;
+}
+
+unsigned    createTextureFromImage(const char* fileName)
+{
+    //1 获取图片格式
+    FREE_IMAGE_FORMAT fifmt = FreeImage_GetFileType(fileName, 0);
+    if (fifmt == FIF_UNKNOWN)
+    {
+        return  0;
+    }
+    //2 加载图片
+    FIBITMAP    *dib = FreeImage_Load(fifmt, fileName,0);
+
+    FREE_IMAGE_COLOR_TYPE type    =   FreeImage_GetColorType(dib);
+
+    //! 获取数据指针
+    FIBITMAP*   temp    =   dib;
+    dib =   FreeImage_ConvertTo32Bits(dib);
+            FreeImage_Unload(temp);
+
+    BYTE*   pixels =   (BYTE*)FreeImage_GetBits(dib);
+    int     width   =   FreeImage_GetWidth(dib);
+    int     height  =   FreeImage_GetHeight(dib);
+
+    for (int i = 0 ;i < width * height * 4 ; i+=4 )
+    {
+        BYTE temp       =   pixels[i];
+        pixels[i]       =   pixels[i + 2];
+        pixels[i + 2]   =   temp;
+    }
+
+    unsigned    res =   createTexture(width,height,pixels);
+    FreeImage_Unload(dib);
+    return      res;
+}
+
+
+ void    onInitGL()
+{
+    _texture    =   createTextureFromImage("1.jpg");
+}
 
 void Render()
 {
@@ -70,14 +107,9 @@ void Render()
     glLoadIdentity();
     glTranslatef(-3,0,-10);
 
-    glScalef(1,1,2);
-
-    static float _angle = 0;
-    _angle += 1;
-    glRotatef(_angle,1,0,0);
 
     glEnable(GL_DEPTH_TEST);
-
+    glEnable(GL_TEXTURE_2D);
     glEnableClientState(GL_VERTEX_ARRAY);
     glEnableClientState(GL_TEXTURE_COORD_ARRAY);
     glVertexPointer(3,GL_FLOAT,         sizeof(Vertex),     &cubeVertices[0].x);
